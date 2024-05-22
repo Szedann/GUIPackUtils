144e4464b6b1564e7f37a95d9d048c888df1c45d
Szedann <65607498+Szedann@users.noreply.github.com>
2024-05-23 00:26:17 +0200

    cleanup

--- fabric/src/main/java/dev/szedann/guipackutils/GUIPackUtils.java
+++ fabric/src/main/java/dev/szedann/guipackutils/GUIPackUtils.java
@@ -1,22 +1,11 @@
 package dev.szedann.guipackutils;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.minecraft.client.Minecraft;
-import net.minecraft.server.packs.resources.PreparableReloadListener;
-import net.minecraft.server.packs.resources.ReloadableResourceManager;
-import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
-import net.minecraft.util.profiling.ProfilerFiller;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 public class GUIPackUtils implements ModInitializer {
-    
+
     @Override
     public void onInitialize() {
-        Constants.LOG.info("Hello Fabric world!");
         CommonClass.init();
     }
 }
